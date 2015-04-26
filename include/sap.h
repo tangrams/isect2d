ed8095d1f7d7c457b3c6b4164b8f20b4ccb52f8a
Varun Talwar <tallytalwar@gmail.com>
2015-04-26 19:43:39 -0400

    SAP improvements
    - insersion of an endPoint in already sorted list
    - sweeping through the sorted list to check for collision pairs
        - as recommended by many sweep and prune references, its benefitial to do a aabb check if a pair collides in one
        axis (avoid doing a sweep and prune in other axes).
    
    TODO:
    I chose std::list implementation for SortListX because of random addition and removal from the list is constant time
    operation!.. However it might perform better if it is a std::vector implementation.
    Another improvement could be to use a std::map of Boxes instead of std::list with key as the pointer to the userData (uniquely defines a label/box/aabb)

--- include/sap.h
+++ include/sap.h
@@ -8,12 +8,18 @@
 
 #define EPSILON 0.0001
 
+/*
+ * NOTE: Implement Boxes as a std::map instead of a std::list
+ * NOTE: Implement SortListX as a std::vector instead of a std::list and compare the performance
+ */
+
 namespace isect2d {
 
     // 8 bytes for end points
     struct EndPoint {
         void* boxID; //same as userData
         float m_value;
+        int m_aabbIndex;
         bool m_isMin = true;
 
         bool operator<(EndPoint _other) {
@@ -28,7 +34,7 @@ namespace isect2d {
             return !(*this < _other);
         }
 
-        EndPoint(void* _boxID, float _m_value, bool _m_isMin) : boxID(_boxID), m_value(_m_value), m_isMin(_m_isMin) {}
+        EndPoint(void* _boxID, float _m_value, int _aabbIndex, bool _m_isMin) : boxID(_boxID), m_value(_m_value), m_aabbIndex(_aabbIndex), m_isMin(_m_isMin) {}
 
     };
     
@@ -37,6 +43,7 @@ namespace isect2d {
     
         std::shared_ptr<EndPoint> m_min; //just do x
         std::shared_ptr<EndPoint> m_max; //just do x
+
 
         Box(void* _boxID, std::shared_ptr<EndPoint> _m_min, std::shared_ptr<EndPoint> _m_max) : boxID(_boxID), m_min(_m_min), m_max(_m_max) {}
 
@@ -49,72 +56,116 @@ namespace isect2d {
 
         SAP() {}
 
-        std::set<std::pair<int, int>> intersect(std::vector<AABB>& _aabbs) {
-            std::set<std::pair<int, int>> pairs;
-            removePoints(_aabbs);
-            updatePoints(_aabbs);
-            sort();
-            intersect();
-            return pairs;
-        }
-
-        void removePoints(std::vector<AABB>& _aabbs) {
-            for(auto& box : Boxes) {
-                const auto aabb = std::find_if(_aabbs.begin(), _aabbs.end(),
-                                            [&](AABB& _aabb) {
-                                                return (_aabb.m_userData == box->boxID);
-                                            });
-
-                Boxes.remove(box);
-                SortListX.remove_if( [&aabb](std::shared_ptr<EndPoint>& _ep) { return( aabb->m_userData == _ep->boxID ); });
-            }
-        }
-
-        void updatePoints(std::vector<AABB>& _aabbs) {
-            for(auto& aabb : _aabbs) {
-
-                auto box = std::find_if(Boxes.begin(), Boxes.end(),
-                                        [&](std::unique_ptr<Box>& box) {
-                                            return (box->boxID == aabb.m_userData);
-                                        });
-                if(box != Boxes.end()) {
-                    (*box)->m_min->m_value = aabb.m_min.x;
-                    (*box)->m_max->m_value = aabb.m_max.x;
-                } else {
-                    addPoint(aabb);
+        void updateSortedPositions(const std::list<std::shared_ptr<EndPoint>>::iterator& _epItr) {
+            if(_epItr != SortListX.begin() && std::next(_epItr) != SortListX.end()) {
+                auto prev = std::prev(_epItr);
+                while(*(*_epItr) < *(*prev)) {
+                    std::swap(*_epItr, *prev);
+                    prev = std::prev(_epItr);
+                }
+                auto next = std::next(_epItr);
+                while(*(*_epItr) > *(*next)) {
+                    std::swap(*_epItr, *next);
+                    next = std::next(_epItr);
+                }
+            } else if(_epItr == SortListX.begin()) {
+                auto next = std::next(_epItr);
+                while(*(*_epItr) > *(*next)) {
+                    std::swap(*_epItr, *next);
+                    next = std::next(_epItr);
+                }
+            } else {
+                auto prev = std::prev(_epItr);
+                while(*(*_epItr) < *(*prev)) {
+                    std::swap(*_epItr, *prev);
+                    prev = std::prev(_epItr);
                 }
             }
         }
 
-        void addPoint(const AABB& _aabb) {
-            std::shared_ptr<EndPoint> ep1(new EndPoint(_aabb.m_userData, _aabb.m_min.x, true));
-            std::shared_ptr<EndPoint> ep2(new EndPoint(_aabb.m_userData, _aabb.m_max.x, false));
+        void addPoint(std::vector<AABB>& _aabbs, std::vector<AABB>::iterator& _aabb) {
+            auto aabbIndex = _aabb - _aabbs.begin();
+            std::shared_ptr<EndPoint> ep1(new EndPoint(_aabb->m_userData, aabbIndex, _aabb->m_min.x, true));
+            std::shared_ptr<EndPoint> ep2(new EndPoint(_aabb->m_userData, aabbIndex, _aabb->m_max.x, false));
             if(*ep1 > *(SortListX.back())) {
                 SortListX.push_back(ep1);
                 SortListX.push_back(ep2);
-            } else {
+            } else if(*ep2 < *(SortListX.front())) {
                 SortListX.push_front(ep2);
                 SortListX.push_front(ep1);
+            } else {
+                SortListX.push_front(ep1);
+                updateSortedPositions(SortListX.begin());
+                SortListX.push_front(ep2);
+                updateSortedPositions(SortListX.begin());
             }
-            std::unique_ptr<Box> box(new Box(_aabb.m_userData, ep1, ep2));
+            std::unique_ptr<Box> box(new Box(_aabb->m_userData, ep1, ep2));
             Boxes.push_back(std::move(box));
         }
 
-        void sort() {
-            for(auto itr = SortListX.begin(); itr != SortListX.end(); itr++) {
-                auto savedEP = *itr;
-                auto jitr = itr;
-                auto prev = jitr;
-                while(jitr != SortListX.begin() && *(*(--prev)) > *savedEP) {
-                    *jitr = *prev;
-                    --jitr;
+        void updatePoints(std::vector<AABB>& _aabbs) {
+            for(auto& box : Boxes) {
+                const auto aabb = std::find_if(_aabbs.begin(), _aabbs.end(),
+                                                [&](AABB& _aabb) {
+                                                    return (_aabb.m_userData == box->boxID);
+                                                });
+
+                if(aabb != _aabbs.end()) {
+                    auto aabbIndex = aabb - _aabbs.begin();
+                    auto itr = std::find_if(SortListX.begin(), SortListX.end(), [&box](std::shared_ptr<EndPoint>& _ep) {return (_ep == box->m_min); });
+                    box->m_min->m_value = aabb->m_min.x;
+                    box->m_min->m_aabbIndex = aabbIndex;
+                    updateSortedPositions(itr);
+                    itr = std::find_if(SortListX.begin(), SortListX.end(), [&box](std::shared_ptr<EndPoint>& _ep) {return (_ep == box->m_max); });
+                    box->m_max->m_value = aabb->m_max.x;
+                    box->m_max->m_aabbIndex = aabbIndex;
+                    updateSortedPositions(itr);
+                } else {
+                    SortListX.remove_if( [&aabb](std::shared_ptr<EndPoint>& _ep) { return (aabb->m_userData == _ep->boxID ); });
+                    Boxes.remove(box);
                 }
-                *jitr = savedEP;
+            }
+
+            for(auto aabbItr = _aabbs.begin(); aabbItr != _aabbs.end(); aabbItr++) {
+                auto box = std::find_if(Boxes.begin(), Boxes.end(),
+                                        [&aabbItr](std::unique_ptr<Box>& box) {
+                                            return (box->boxID == aabbItr->m_userData);
+                                        });
+
+                if(box == Boxes.end()) {
+                    addPoint(_aabbs, aabbItr);
+                }
+            }
+            
+        }
+
+        void calPairs(std::vector<AABB>& _aabbs, std::set<std::pair<int, int>>& pairs) {
+            for(auto itr = SortListX.begin(); itr != SortListX.end(); itr++) {
+                auto& ep1 = *itr;
+                if(!(ep1->m_isMin)) {
+                    continue;
+                } else {
+                    for(auto jitr = std::next(itr); jitr != SortListX.end(); jitr++) {
+                        auto& ep2 = *jitr;
+                        if(ep1->boxID == ep2->boxID) {
+                            break;
+                        } else if(!(ep2->m_isMin)) {
+                            continue;
+                        } else {
+                            if (_aabbs[ep1->m_aabbIndex].intersect(_aabbs[ep2->m_aabbIndex])) {
+                                pairs.insert({ep1->m_aabbIndex, ep2->m_aabbIndex });
+                            }
+                        }
+                    }
+                }
             }
         }
 
-        void intersect() {
-
+        std::set<std::pair<int, int>> intersect(std::vector<AABB>& _aabbs) {
+            std::set<std::pair<int, int>> pairs;
+            updatePoints(_aabbs);
+            calPairs(_aabbs, pairs);
+            return pairs;
         }
 
     private:
